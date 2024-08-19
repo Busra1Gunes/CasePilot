047deaac0ad50443270228f80c386d61717db3e7
Büşra Güneş <121101278+Busra1Gunes@users.noreply.github.com>
2024-08-19 21:59:56 +0300

    Add project files.

---
+++ ConsoleUI/Context.cs
@@ -0,0 +1,24 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-IUMMNFO\SQLEXPRESS;Initial Catalog=StockWise;Integrated Security=True;Trust Server Certificate=True");
+
+		}
+		DbSet<User> Users { get; set; }
+		DbSet<Stock> Stocks { get; set; }
+		DbSet<Supplier> Suppliers { get; set; }
+
+	}
+}
---
+++ DataAccess/Abstract/IRepositoryDal.cs
@@ -0,0 +1,17 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+	public interface IRepositoryDal<T> where T : class, IEntity, new()
+	{
+		List<T> GetAll();
+		T Get();
+		
+
+	}
+}
